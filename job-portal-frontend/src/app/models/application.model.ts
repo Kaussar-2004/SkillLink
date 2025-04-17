f7ac41ee0f18266640e170f33c7b0969aa727ae4
Kaussar A <159014613+Kaussar-2004@users.noreply.github.com>
2025-04-17 23:59:59 +0530

    delete

--- job-portal-frontend/src/app/models/application.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Application {
-  id?: number;
-  job_id: number;
-  applicant_name: string;
-  email: string;
-  applied_at?: string;
-  job_title?: string;
-  company_name?: string;
-}
