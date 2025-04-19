55ea8144add2ea18a2b2d62c0a92627283a44d4f
Kaussar A <159014613+Kaussar-2004@users.noreply.github.com>
2025-04-19 21:22:21 +0530

    Update server.js

--- job-portal-backend/server.js
+++ job-portal-backend/server.js
@@ -6,7 +6,8 @@ const jobRoutes = require('./routes/jobRoutes');
 const applicationRoutes = require('./routes/applicationroutes');
 
 // Middleware
-app.use(cors());
+app.use(cors(origin: 'http://localhost:4200', // or '*' for testing
+  credentials: true));
 app.use(express.json());
 
 // Request logging middleware
